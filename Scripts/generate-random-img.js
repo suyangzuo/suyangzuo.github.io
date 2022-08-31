7ae9aaae0f7cee7d21cf94ff5469d35f2793c7f5
suyangzuo <suyangzuo@163.com>
2022-08-31 14:04:25 +0800

    改版了主页，修改贡献者页面位置，增加了Web前端卡片

--- Scripts/generate-random-img.js
+++ Scripts/generate-random-img.js
@@ -1,55 +1,55 @@
 var techStackLogos = [
   {
     name: "Angular",
-    img: "Images/TechStack-Logos/Angular-logo.png",
+    img: "/Images/TechStack-Logos/Angular-logo.png",
   },
   {
     name: "C",
-    img: "Images/TechStack-Logos/C-logo.png",
+    img: "/Images/TechStack-Logos/C-logo.png",
   },
   {
     name: "CSharp",
-    img: "Images/TechStack-Logos/CS-logo.png",
+    img: "/Images/TechStack-Logos/CS-logo.png",
   },
   {
     name: "Python",
-    img: "Images/TechStack-Logos/Python-Logo.png",
+    img: "/Images/TechStack-Logos/Python-Logo.png",
   },
   {
     name: "DataStructure",
-    img: "Images/TechStack-Logos/DataStructure-logo.png",
+    img: "/Images/TechStack-Logos/DataStructure-logo.png",
   },
   {
     name: "Java",
-    img: "Images/TechStack-Logos/Java-logo.png",
+    img: "/Images/TechStack-Logos/Java-logo.png",
   },
   {
     name: "MySQL",
-    img: "Images/TechStack-Logos/MySQL-logo.png",
+    img: "/Images/TechStack-Logos/MySQL-logo.png",
   },
   {
     name: "React",
-    img: "Images/TechStack-Logos/React-logo.png",
+    img: "/Images/TechStack-Logos/React-logo.png",
   },
   {
     name: "Vue",
-    img: "Images/TechStack-Logos/Vue-logo.png",
+    img: "/Images/TechStack-Logos/Vue-logo.png",
   },
   {
     name: "Git",
-    img: "Images/TechStack-Logos/Git-Logo.png",
+    img: "/Images/TechStack-Logos/Git-Logo.png",
   },
   {
     name: "SQL-Server",
-    img: "Images/TechStack-Logos/SQL-Server-Logo.png",
+    img: "/Images/TechStack-Logos/SQL-Server-Logo.png",
   },
   {
     name: "SQLite",
-    img: "Images/TechStack-Logos/SQLite-Logo.png",
+    img: "/Images/TechStack-Logos/SQLite-Logo.png",
   },
   {
     name: "PostgreSQL",
-    img: "Images/TechStack-Logos/PostgreSQL-Logo.png",
+    img: "/Images/TechStack-Logos/PostgreSQL-Logo.png",
   },
 ];
 
