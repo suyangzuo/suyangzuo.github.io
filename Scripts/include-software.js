a952fe74860c46cf2a8cbd45f92cc5555563814f
suyangzuo <suyangzuo@163.com>
2022-10-15 14:35:43 +0800

    压缩了很多图片

--- Scripts/include-software.js
+++ Scripts/include-software.js
@@ -1,10 +1,12 @@
-const categorys = document.getElementsByClassName("software-category");
-const containers = document.getElementsByClassName("software-container");
+const categorys = document.getElementsByClassName("software-category"); //获取软件选择列表
+const containers = document.getElementsByClassName("software-container"); //获取软件卡片容器
 
+//如果软件名为空（每次浏览器会话第1次进入软件页），则默认为“文本编辑器”
 if (sessionStorage.getItem("software-name") === null) {
   sessionStorage.setItem("software-name", "文本编辑器");
 }
 
+//进入软件页时，将每个软件选项设置为默认样式，并将与会话中保存名称相同的软件选项设置为绿底金字
 Array.from(categorys).forEach((category) => {
   category.style.background = "none";
   let p = category.firstElementChild;
@@ -15,8 +17,20 @@ Array.from(categorys).forEach((category) => {
   }
 });
 
+//通过会话中保存的软件名，获取对应文件名的HTML文件
 let fileName = getSoftwareFileName(sessionStorage.getItem("software-name"));
 
+//获取文件名对应的HTML文件内容，并置入“software-container"标签内
+fetch(fileName)
+  .then(async (response) => await response.text())
+  .then((content) => (containers[0].innerHTML = content));
+
+Array.from(categorys).forEach((category) => {
+  category.addEventListener("click", insertSoftwareHTML);
+  category.addEventListener("click", changeCategoryStyle);
+});
+
+//通过软件名，获取对应文件名的HTML文件
 function getSoftwareFileName(softwareName) {
   let tempFileName = "";
   switch (softwareName) {
@@ -54,15 +68,7 @@ function getSoftwareFileName(softwareName) {
   return tempFileName;
 }
 
-fetch(fileName)
-  .then(async (response) => await response.text())
-  .then((content) => (containers[0].innerHTML = content));
-
-Array.from(categorys).forEach((category) => {
-  category.addEventListener("click", insertSoftwareHTML);
-  category.addEventListener("click", changeCategoryStyle);
-});
-
+//每次点击软件选项，通过点击的软件名，获取对应的HTML文件内容并置入“software-container"标签内
 async function insertSoftwareHTML() {
   if (
     sessionStorage.getItem("software-name") === this.firstElementChild.innerText
@@ -80,6 +86,7 @@ async function insertSoftwareHTML() {
     .then((content) => (containers[0].innerHTML = content));
 }
 
+//每次点击软件选项，通过点击的软件名，设置其为绿底金字，其它选项置为默认样式
 function changeCategoryStyle() {
   Array.from(categorys).forEach((category) => {
     category.style.background = "none";
