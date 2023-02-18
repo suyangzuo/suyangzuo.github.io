851fef87a6dadb44be766e44ee1be6bed0f0e5ae
suyangzuo <suyangzuo@163.com>
2023-02-18 21:13:39 +0800

    调整了图片长廊逻辑代码，不可行，待废弃

--- Scripts/contributors-gallery.js
+++ Scripts/contributors-gallery.js
@@ -24,29 +24,46 @@ let edgeWidth = parseFloat(rootStyle.getPropertyValue("--edge-width"));
 const sliderContent = slider1.innerHTML;
 slider2.innerHTML = sliderContent;
 
-// slider1.setAttribute("style", `transform: translateX(${edgeWidth}px)`);
 let slider1ImageIndex = 0;
 let slider2ImageIndex = -1;
 let totalOffset1 = 0;
 let totalOffset2 = 0;
+let slider1Actived = true;
+let slider2Actived = false;
+let nextTimes1 = 0;
+let nextTimes2 = 0;
 
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
     const buttonType = button.dataset.buttonType;
     if (buttonType === "next") {
-      if (slider1ImageIndex < 9) {
-        slider1ImageIndex++;
-        if (slider1ImageIndex === 8) {
-          slider2.style.right = `calc(
-            (${imageWidth}px + ${imageGap}px) * ${imageCount - 2} -
-              ${edgeWidth}px - ${imageGap}px
-          )`;
-        }
+      if (slider1Actived && slider1ImageIndex === 9) {
+        slider2ImageIndex = -1;
+        slider2Actived = true;
+        slider1Actived = false;
+        nextTimes2 += 2;
+      }
+      if (slider2Actived && slider2ImageIndex === 9) {
+        slider1ImageIndex = -1;
+        slider1Actived = true;
+        slider2Actived = false;
+        nextTimes1 += 2;
+      }
+      if (slider1Actived && slider1ImageIndex < 9) slider1ImageIndex++;
+      if (slider2Actived && slider2ImageIndex < 9) slider2ImageIndex++;
+      console.log(slider2ImageIndex);
+
+      if (slider1ImageIndex === 2) {
+        slider2.style.left =
+          "calc(var(--slider-width) + var(--edge-width) + var(--image-gap))";
+      }
+      if (slider2ImageIndex === 2) {
+        slider1.style.left =
+          "calc((var(--slider-width) + var(--edge-width) + var(--image-gap)) * 2 - var(--edge-width))";
       }
     } else {
-      if (slider1ImageIndex > 0) {
-        slider1ImageIndex--;
-      }
+      slider1ImageIndex--;
+      slider2ImageIndex--;
     }
 
     let offset =
@@ -54,12 +71,12 @@ buttons.forEach((button) => {
     totalOffset1 += offset;
     totalOffset2 += offset;
 
-    slider1.setAttribute("style", `transform: translateX(${totalOffset1}px)`);
-    slider2.setAttribute("style", `transform: translateX(${totalOffset2}px)`);
+    slider1.style.transform = `translateX(${totalOffset2}px)`;
+    slider2.style.transform = `translateX(${totalOffset2}px)`;
   });
 });
 
-window.onresize = () => {
-  galleryWidth = parseFloat(rootStyle.getPropertyValue("--gallery-width"));
-  edgeWidth = parseFloat(rootStyle.getPropertyValue("--edge-width"));
-};
+// window.onresize = () => {
+//   galleryWidth = parseFloat(rootStyle.getPropertyValue("--gallery-width"));
+//   edgeWidth = parseFloat(rootStyle.getPropertyValue("--edge-width"));
+// };
