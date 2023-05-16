ed1d8e17c77527e26b711bc9c73b782d62ba9ae9
suyangzuo <suyangzuo@163.com>
2023-05-16 11:19:43 +0800

    为图像长廊添加自动滚动逻辑，底栏添加图片版权声明

--- Scripts/index.js
+++ Scripts/index.js
@@ -8,17 +8,42 @@ const 箭头组 = document.getElementsByClassName("箭头");
 const 左箭头 = 箭头组[0];
 const 右箭头 = 箭头组[1];
 
+const 自动滚动延时 = 4000;
 const 图像数量 = 10;
 const 图像移动时长 = 350;
 const 图像容器宽度 = 800;
 
-const 初始图像图像位移 = 400;
-let 图像位移 = 初始图像图像位移; // --> 图像容器宽度/2
+const 初始图像位移 = 400;
+let 图像位移 = 初始图像位移; // --> 图像容器宽度 / 2
 const 初始图像索引 = 5;
 let 图像索引 = 初始图像索引; // --> 中间滑块的第5张图
 
+let mouseIsOver = false; // --> 判断鼠标是否悬停在箭头上
+
 左箭头.addEventListener("click", 点击左箭头);
 右箭头.addEventListener("click", 点击右箭头);
+
+let 图像长廊定时滚动 = setInterval(点击右箭头, 自动滚动延时);
+
+左箭头.addEventListener("mouseenter", () => {
+  mouseIsOver = true;
+  clearInterval(图像长廊定时滚动);
+});
+
+左箭头.addEventListener("mouseleave", () => {
+  mouseIsOver = false;
+  图像长廊定时滚动 = setInterval(点击右箭头, 自动滚动延时);
+});
+
+右箭头.addEventListener("mouseenter", () => {
+  mouseIsOver = true;
+  clearInterval(图像长廊定时滚动);
+});
+
+右箭头.addEventListener("mouseleave", () => {
+  mouseIsOver = false;
+  图像长廊定时滚动 = setInterval(点击右箭头, 自动滚动延时);
+});
 
 function 点击左箭头() {
   左箭头.removeEventListener("click", 点击左箭头);
@@ -26,14 +51,15 @@ function 点击左箭头() {
   图像位移 += 图像容器宽度;
   图像滑块组.style.transition = `transform ${图像移动时长}ms ease-out, left 250ms ease-out`;
   图像滑块组.style.transform = `translateX(${图像位移}px)`;
-  左箭头.style.filter = "brightness(50%)";
+  if (mouseIsOver) {
+    左箭头.style.filter = "brightness(50%)";
+  }
 
   if (图像索引 <= 初始图像索引 - 图像数量) {
     setTimeout(() => {
-      图像索引 = 5;
-      图像位移 = 初始图像图像位移;
-      图像滑块组.style.transition = "transform 0ms, left 250ms ease-out";
-      图像滑块组.style.transform = `translateX(${图像位移}px)`;
+      图像索引 = 初始图像索引;
+      图像位移 = 初始图像位移;
+      图像长廊位移重置();
     }, 图像移动时长);
   }
 
@@ -49,14 +75,15 @@ function 点击右箭头() {
   图像位移 -= 图像容器宽度;
   图像滑块组.style.transition = `transform ${图像移动时长}ms ease-out, left 250ms ease-out`;
   图像滑块组.style.transform = `translateX(${图像位移}px)`;
-  右箭头.style.filter = "brightness(50%)";
+  if (mouseIsOver) {
+    右箭头.style.filter = "brightness(50%)";
+  }
 
   if (图像索引 >= 初始图像索引 + 图像数量) {
     setTimeout(() => {
-      图像索引 = 5;
-      图像位移 = 初始图像图像位移;
-      图像滑块组.style.transition = "transform 0ms, left 250ms ease-out";
-      图像滑块组.style.transform = `translateX(${图像位移}px)`;
+      图像索引 = 初始图像索引;
+      图像位移 = 初始图像位移;
+      图像长廊位移重置();
     }, 图像移动时长);
   }
 
@@ -64,4 +91,9 @@ function 点击右箭头() {
     右箭头.addEventListener("click", 点击右箭头);
     右箭头.style.filter = "brightness(100%)";
   }, 图像移动时长);
+}
+
+function 图像长廊位移重置() {
+  图像滑块组.style.transition = "transform 0ms, left 250ms ease-out";
+  图像滑块组.style.transform = `translateX(${初始图像位移}px)`;
 }
