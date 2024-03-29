3a06fd1df6618d33c669203969e5b26602b48a44
suyangzuo <suyangzuo@163.com>
2024-03-29 16:39:38 +0800

    更换主页大量图片，更换宗旨页背景并删除原分区背景图片，添加输入字符串博客的部分框架

--- public/Blogs/blog-index.js
+++ public/Blogs/blog-index.js
@@ -666,6 +666,8 @@ function 特殊元素样式补充() {
       const 修剪文本 = 前一节点.textContent.trim();
       if (修剪文本 === "" || 远距标点组.some((标点) => 标点 === 修剪文本)) {
         行内代码.style.marginLeft = "0";
+      } else {
+        行内代码.style.marginLeft = "0.25em";
       }
     }
 
