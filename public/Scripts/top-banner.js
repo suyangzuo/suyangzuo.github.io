5f1a5b6ba7d611990f4cf09474bc8420ce4cf9f3
suyangzuo <suyangzuo@163.com>
2024-06-28 21:23:24 +0800

    移除顶栏搜索与登录，将GitHub与捐赠独立到顶栏

--- public/Scripts/top-banner.js
+++ public/Scripts/top-banner.js
@@ -37,26 +37,7 @@ const 导航1级文本组 = document.querySelectorAll(".导航1级文本");
   });
 });
 
-window.addEventListener("load", () => {
-  const 删除搜索内容按钮 =
-    document.getElementsByClassName("删除搜索内容按钮")[0];
-  const 搜索框 = document.getElementById("搜索框");
-  搜索框.addEventListener("input", () => {
-    if (搜索框.value === "") {
-      删除搜索内容按钮.style.visibility = "hidden";
-    } else {
-      删除搜索内容按钮.style.visibility = "visible";
-    }
-  });
-
-  删除搜索内容按钮.addEventListener("click", () => {
-    搜索框.value = "";
-    搜索框.focus();
-    删除搜索内容按钮.style.visibility = "hidden";
-  });
-});
-
-const 捐赠按钮 = document.getElementById("捐赠按钮");
+const 捐赠按钮 = document.getElementById("捐赠");
 捐赠按钮.addEventListener("click", 打开捐赠界面);
 
 function 打开捐赠界面() {
