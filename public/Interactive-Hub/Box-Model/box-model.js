baf1dcec40e14b9f559fa0911642493d92764666
suyangzuo <suyangzuo@163.com>
2024-12-16 14:24:00 +0800

    重写盒子模型中心的4层盒子的尺寸逻辑

--- public/Interactive-Hub/Box-Model/box-model.js
+++ public/Interactive-Hub/Box-Model/box-model.js
@@ -1,130 +1,66 @@
-const 展示区 = document.querySelector(".展示区");
-const 外边距盒子 = 展示区.querySelector(".外边距盒子");
-const 边框盒子 = 展示区.querySelector(".边框盒子");
-const 内容盒子 = 展示区.querySelector(".内容盒子");
 const root = document.querySelector(":root");
-const rootStyle = window.getComputedStyle(root);
-const 初始外边距尺寸 = rootStyle.getPropertyValue("--外边距尺寸");
-const 初始内边距尺寸 = rootStyle.getPropertyValue("--内边距尺寸");
-const 初始边框尺寸 = rootStyle.getPropertyValue("--边框尺寸");
-const 初始内容尺寸 = rootStyle.getPropertyValue("--内容尺寸");
+const 滑块组 = document.querySelectorAll("input[type='range']");
+const 内容盒子单选框 = document.querySelector("#内容盒子");
+const 边框盒子单选框 = document.querySelector("#边框盒子");
+const 盒子 = document.querySelector(".边框盒子");
 
-const 操作区 = document.querySelector(".操作区");
-const 内容盒子单选框 = 操作区.querySelector("#内容盒子");
-const 边框盒子单选框 = 操作区.querySelector("#边框盒子");
-const 宽度滑块 = 操作区.querySelector("#宽度");
-const 高度滑块 = 操作区.querySelector("#高度");
-const 内容滑块 = 操作区.querySelector("#内容");
-const 内边距滑块 = 操作区.querySelector("#内边距");
-const 边框滑块 = 操作区.querySelector("#边框");
-const 外边距滑块 = 操作区.querySelector("#外边距");
-const 模型设置区 = 操作区.querySelector(".模型设置区");
-const 模型滑块组 = 模型设置区.querySelectorAll(".范围滑块");
-const 盒子尺寸中文 = 操作区.querySelector(".盒子尺寸中文");
+const 宽度滑块 = document.querySelector("#宽度");
+const 高度滑块 = document.querySelector("#高度");
+const 宽高关联复选框 = document.querySelector("#宽高关联");
+const 内边距滑块 = document.querySelector("#内边距");
+const 边框滑块 = document.querySelector("#边框");
+const 外边距滑块 = document.querySelector("#外边距");
 
-外边距盒子.style.width = `calc(${window.getComputedStyle(边框盒子).width} + ${初始外边距尺寸} * 2)`;
-外边距盒子.style.height = `calc(${window.getComputedStyle(边框盒子).height} + ${初始外边距尺寸} * 2)`;
+const 盒子尺寸中文 = document.querySelector(".盒子尺寸中文");
 
-内容盒子单选框.addEventListener("change", () => {
-  盒子尺寸中文.textContent = "内容尺寸";
-  设置宽高元素属性();
-});
+内容盒子单选框.addEventListener("change", 更新盒子模型时刷新所有属性);
+边框盒子单选框.addEventListener("change", 更新盒子模型时刷新所有属性);
 
-边框盒子单选框.addEventListener("change", () => {
-  盒子尺寸中文.textContent = "盒子尺寸";
-  设置宽高元素属性();
-});
-
-function 设置宽高元素属性() {
-  if (边框盒子单选框.checked) {
-    宽度滑块.min = "150";
-    宽度滑块.max = "750";
-    高度滑块.min = "150";
-    高度滑块.max = "750";
-    宽度滑块.value = parseInt(window.getComputedStyle(边框盒子).width, 10);
-    高度滑块.value = parseInt(window.getComputedStyle(边框盒子).height, 10);
-  } else {
-    宽度滑块.min = "150";
-    宽度滑块.max = "350";
-    高度滑块.min = "150";
-    高度滑块.max = "350";
-    宽度滑块.value = parseInt(window.getComputedStyle(内容盒子).width, 10);
-    高度滑块.value = parseInt(window.getComputedStyle(内容盒子).height, 10);
-  }
-  const 宽度修剪百分比 = 获取滑块背景修剪百分比(宽度滑块);
-  const 高度修剪百分比 = 获取滑块背景修剪百分比(高度滑块);
-  root.style.setProperty("--宽度修剪比例", 宽度修剪百分比);
-  root.style.setProperty("--高度修剪比例", 高度修剪百分比);
-  拖动滑块时修改数字(宽度滑块);
-  拖动滑块时修改数字(高度滑块);
-}
-
-宽度滑块.addEventListener("input", (event) => {
-  const 百分比 = 获取滑块背景修剪百分比(event.target);
-  root.style.setProperty("--宽度修剪比例", 百分比);
-  root.style.setProperty("--内容盒子宽度", `${event.target.value}px`);
-  设置外边距盒子尺寸();
-  拖动滑块时修改数字(event.target);
-});
-
-高度滑块.addEventListener("input", (event) => {
-  const 百分比 = 获取滑块背景修剪百分比(event.target);
-  root.style.setProperty("--高度修剪比例", 百分比);
-  root.style.setProperty("--内容盒子高度", `${event.target.value}px`);
-  设置外边距盒子尺寸();
-  拖动滑块时修改数字(event.target);
-});
-
-内容滑块.addEventListener("input", (event) => {
-  const 百分比 = 获取滑块背景修剪百分比(event.target);
-  root.style.setProperty("--内容修剪比例", 百分比);
-  root.style.setProperty("--内容尺寸", `${event.target.value}px`);
-  root.style.setProperty("--内容盒子宽度", `${event.target.value}px`);
-  root.style.setProperty("--内容盒子高度", `${event.target.value}px`);
-  if (内容盒子单选框.checked) {
-    宽度滑块.value = event.target.value;
-    高度滑块.value = event.target.value;
-    const 宽度百分比 = 获取滑块背景修剪百分比(宽度滑块);
-    const 高度百分比 = 获取滑块背景修剪百分比(高度滑块);
-    root.style.setProperty("--宽度修剪比例", 宽度百分比);
-    root.style.setProperty("--高度修剪比例", 高度百分比);
-    拖动滑块时修改数字(宽度滑块);
+宽度滑块.addEventListener("input", () => {
+  root.style.setProperty("--盒子宽度", `${宽度滑块.value}px`);
+  if (宽高关联复选框.checked) {
+    高度滑块.value = 宽度滑块.value;
+    root.style.setProperty("--盒子高度", `${高度滑块.value}px`);
+    更新滑块背景修剪百分比(高度滑块);
     拖动滑块时修改数字(高度滑块);
   }
-  设置外边距盒子尺寸();
-  拖动滑块时修改数字(event.target);
 });
 
-内边距滑块.addEventListener("input", (event) => {
-  const 百分比 = 获取滑块背景修剪百分比(event.target);
-  root.style.setProperty("--内边距修剪比例", 百分比);
-  root.style.setProperty("--内边距尺寸", `${event.target.value}px`);
-  设置外边距盒子尺寸();
-  拖动滑块时修改数字(event.target);
+高度滑块.addEventListener("input", () => {
+  root.style.setProperty("--盒子高度", `${高度滑块.value}px`);
+  if (宽高关联复选框.checked) {
+    宽度滑块.value = 高度滑块.value;
+    root.style.setProperty("--盒子宽度", `${宽度滑块.value}px`);
+    更新滑块背景修剪百分比(宽度滑块);
+    拖动滑块时修改数字(宽度滑块);
+  }
 });
 
-边框滑块.addEventListener("input", (event) => {
-  const 百分比 = 获取滑块背景修剪百分比(event.target);
-  root.style.setProperty("--边框修剪比例", 百分比);
-  root.style.setProperty("--边框尺寸", `${event.target.value}px`);
-  设置外边距盒子尺寸();
-  拖动滑块时修改数字(event.target);
+内边距滑块.addEventListener("input", () => {
+  root.style.setProperty("--内边距尺寸", `${内边距滑块.value}px`);
 });
 
-外边距滑块.addEventListener("input", (event) => {
-  const 百分比 = 获取滑块背景修剪百分比(event.target);
-  root.style.setProperty("--外边距修剪比例", 百分比);
-  root.style.setProperty("--外边距尺寸", `${event.target.value}px`);
-  设置外边距盒子尺寸();
-  拖动滑块时修改数字(event.target);
+边框滑块.addEventListener("input", () => {
+  root.style.setProperty("--边框尺寸", `${边框滑块.value}px`);
 });
 
-function 获取滑块背景修剪百分比(滑块) {
+外边距滑块.addEventListener("input", () => {
+  root.style.setProperty("--外边距尺寸", `${外边距滑块.value}px`);
+});
+
+for (const 滑块 of 滑块组) {
+  滑块.addEventListener("input", () => {
+    更新滑块背景修剪百分比(滑块);
+    拖动滑块时修改数字(滑块);
+  });
+}
+
+function 更新滑块背景修剪百分比(滑块) {
   const 滑块值 = parseInt(滑块.value, 10);
   const 最小值 = parseInt(滑块.min, 10);
   const 最大值 = parseInt(滑块.max, 10);
   const 滑块比例 = (滑块值 - 最小值) / (最大值 - 最小值);
-  return `${100 - 滑块比例 * 100}%`;
+  root.style.setProperty(`--${滑块.id}修剪比例`, `${100 - 滑块比例 * 100}%`);
 }
 
 function 拖动滑块时修改数字(滑块) {
@@ -132,15 +68,23 @@ function 拖动滑块时修改数字(滑块) {
   数字.textContent = 滑块.value;
 }
 
-function 设置外边距盒子尺寸() {
-  const 实时根元素样式 = window.getComputedStyle(root);
-  const 内容尺寸 = 实时根元素样式.getPropertyValue("--内容尺寸");
-  const 内容宽度 = 实时根元素样式.getPropertyValue("--内容盒子宽度");
-  const 内容高度 = 实时根元素样式.getPropertyValue("--内容盒子高度");
-  const 内边距尺寸 = 实时根元素样式.getPropertyValue("--内边距尺寸");
-  const 边框尺寸 = 实时根元素样式.getPropertyValue("--边框尺寸");
-  const 外边距尺寸 = 实时根元素样式.getPropertyValue("--外边距尺寸");
+function 更新盒子模型() {
+  盒子.style.boxSizing = 内容盒子单选框.checked ? "content-box" : "border-box";
+  盒子尺寸中文.textContent = 内容盒子单选框.checked ? "内容尺寸" : "盒子尺寸";
+}
 
-  外边距盒子.style.width = `calc(${内容宽度} + ${内边距尺寸} * 2 + ${边框尺寸} * 2 + ${外边距尺寸} * 2)`;
-  外边距盒子.style.height = `calc(${内容高度} + ${内边距尺寸} * 2 + ${边框尺寸} * 2 + ${外边距尺寸} * 2)`;
+function 更新宽高滑块属性() {
+  宽度滑块.max = 内容盒子单选框.checked ? "350" : "750";
+  高度滑块.max = 内容盒子单选框.checked ? "350" : "750";
+}
+
+function 更新盒子模型时刷新所有属性() {
+  更新盒子模型();
+  更新宽高滑块属性();
+  更新滑块背景修剪百分比(宽度滑块);
+  更新滑块背景修剪百分比(高度滑块);
+  拖动滑块时修改数字(宽度滑块);
+  拖动滑块时修改数字(高度滑块);
+  root.style.setProperty("--盒子宽度", `${宽度滑块.value}px`);
+  root.style.setProperty("--盒子高度", `${高度滑块.value}px`);
 }
