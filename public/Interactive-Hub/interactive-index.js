2ffd26e7e85957a65fac7fe9c2ebc6d99dfdb761
suyangzuo <suyangzuo@163.com>
2024-12-20 13:24:31 +0800

    交互中心主页卡片hover样式改为四角线框；调整排序算法滑块的样式；调整贡献者中关于创始人的部分描述

--- public/Interactive-Hub/interactive-index.js
+++ public/Interactive-Hub/interactive-index.js
@@ -1,3 +1,4 @@
+const root = document.querySelector(":root");
 const 交互列表组 = document.querySelectorAll(".交互列表");
 
 for (const 交互列表 of 交互列表组) {
@@ -8,4 +9,27 @@ for (const 交互列表 of 交互列表组) {
     const 序号 = 交互单项.querySelector(".序号");
     序号.innerText = index + 1;
   }
+}
+
+const 交互单向组 = document.querySelectorAll(".交互单项");
+for (const 交互单项 of 交互单向组) {
+  交互单项.addEventListener("mouseenter", () => {
+    const 尺寸 = {
+      宽: `${交互单项.offsetWidth}px`,
+      高: `${交互单项.offsetHeight}px`,
+    };
+
+    const 坐标 = {
+      x: `${交互单项.offsetLeft}px`,
+      y: `${交互单项.offsetTop}px`,
+    };
+
+    const 标题 =
+      交互单项.parentElement.parentElement.querySelector("h1").textContent;
+
+    root.style.setProperty("--交互单项宽度", 尺寸.宽);
+    root.style.setProperty("--交互单项高度", 尺寸.高);
+    root.style.setProperty(`--${标题}-水平偏移`, 坐标.x);
+    root.style.setProperty(`--${标题}-垂直偏移`, 坐标.y);
+  });
 }
