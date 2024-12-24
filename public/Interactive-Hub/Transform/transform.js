b7810bc48063240c72ccd2367bf05a8e072974fa
suyangzuo <suyangzuo@163.com>
2024-12-24 16:45:10 +0800

    为变换中心自身属性添加单独的重置按钮，为缩放添加水平垂直关联

--- public/Interactive-Hub/Transform/transform.js
+++ public/Interactive-Hub/Transform/transform.js
@@ -15,6 +15,9 @@ const 旋转滑块组 = document.querySelectorAll(".旋转区 .滑块");
 const 变换滑块组 = [...平移滑块组, ...缩放滑块组, ...旋转滑块组];
 const 选择器组 = document.querySelectorAll(".选择器");
 
+const 重置变换属性按钮组 = document.querySelectorAll(".控件区 .按钮");
+const 水平垂直关联复选框 = document.querySelector("#水平垂直关联");
+
 let 水平平移单位 = "%";
 let 垂直平移单位 = "%";
 
@@ -34,11 +37,15 @@ for (const 选择器 of 选择器组) {
 
 for (const 滑块 of 滑块组) {
   滑块.addEventListener("input", () => {
-    const percentValue = 获取滑块填充百分比(滑块);
-    root.style.setProperty(`--${滑块.id}填充`, `${percentValue}%`);
-    const 控件数字 = 滑块.nextElementSibling.querySelector(".控件数字");
-    控件数字.textContent = 滑块.value;
+    设置滑块填充与数值(滑块);
   });
+}
+
+function 设置滑块填充与数值(滑块) {
+  const percentValue = 获取滑块填充百分比(滑块);
+  root.style.setProperty(`--${滑块.id}填充`, `${percentValue}%`);
+  const 控件数字 = 滑块.nextElementSibling.querySelector(".控件数字");
+  控件数字.textContent = 滑块.value;
 }
 
 for (const 单选框 of 变换风格单选组) {
@@ -84,13 +91,25 @@ for (const 滑块 of 平移滑块组) {
 for (const 滑块 of 缩放滑块组) {
   滑块.addEventListener("input", () => {
     root.style.setProperty(`--${滑块.id}`, `${滑块.value}%`);
+    if (水平垂直关联复选框.checked) {
+      const 另一滑块 = Array.from(缩放滑块组).find(
+        (checkbox) => checkbox.id !== 滑块.id,
+      );
+      另一滑块.value = 滑块.value;
+      root.style.setProperty(`--${另一滑块.id}`, `${另一滑块.value}%`);
+      设置滑块填充与数值(另一滑块);
+    }
     显示或隐藏原始盒子();
-    const 水平缩放 = parseInt(rootStyle.getPropertyValue("--缩放-水平"), 10);
-    const 垂直缩放 = parseInt(rootStyle.getPropertyValue("--缩放-垂直"), 10);
-    const 原点水平缩放 = `${10000 / 水平缩放}%`;
-    const 原点垂直缩放 = `${10000 / 垂直缩放}%`;
-    原点盒子.style.scale = `${原点水平缩放} ${原点垂直缩放}`;
+    刷新原点缩放();
   });
+}
+
+function 刷新原点缩放() {
+  const 水平缩放 = parseInt(rootStyle.getPropertyValue("--缩放-水平"), 10);
+  const 垂直缩放 = parseInt(rootStyle.getPropertyValue("--缩放-垂直"), 10);
+  const 原点水平缩放 = `${10000 / 水平缩放}%`;
+  const 原点垂直缩放 = `${10000 / 垂直缩放}%`;
+  原点盒子.style.scale = `${原点水平缩放} ${原点垂直缩放}`;
 }
 
 for (const 滑块 of 旋转滑块组) {
@@ -113,6 +132,42 @@ function 显示或隐藏原始盒子() {
   } else {
     原始盒子.classList.remove("显示原始盒子");
   }
+}
+
+for (const 按钮 of 重置变换属性按钮组) {
+  按钮.addEventListener("click", () => {
+    const 本区滑块组 =
+      按钮.parentElement.parentElement.parentElement.querySelectorAll(".滑块");
+
+    for (const 滑块 of 本区滑块组) {
+      const min = parseInt(滑块.min, 10);
+      const max = parseInt(滑块.max, 10);
+      滑块.value = max - (max - min) / 2;
+      let 单位 = "%";
+      if (滑块.id.includes("旋转")) {
+        单位 = "deg";
+      } else if (滑块.id.includes("平移")) {
+        滑块.nextElementSibling.querySelector(".选择器").value = "%";
+      }
+      root.style.setProperty(`--${滑块.id}`, `${滑块.value}${单位}`);
+      设置滑块填充与数值(滑块);
+      if (滑块.id.includes("缩放")) {
+        刷新原点缩放();
+      }
+    }
+
+    if (按钮.id === "重置变换原点") {
+      for (const 滑块 of 原点滑块组) {
+        const min = parseInt(滑块.min, 10);
+        const max = parseInt(滑块.max, 10);
+        滑块.value = max - (max - min) / 2;
+        root.style.setProperty(`--${滑块.id}偏移`, `${滑块.value}%`);
+        设置滑块填充与数值(滑块);
+      }
+    }
+
+    显示或隐藏原始盒子();
+  });
 }
 
 const 重置按钮 = document.querySelector(".重置按钮");
