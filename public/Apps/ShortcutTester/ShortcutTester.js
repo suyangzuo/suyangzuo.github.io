1e23a1f734df10fc51054df27b756807136fb09e
suyangzuo <suyangzuo@163.com>
2025-01-03 16:28:37 +0800

    部分修复因CSS直接在元素上使用自定义光标导致和FontAwesome及LordIcon冲突的问题；为快捷键测试器添加取消选择、取消测试，优化了快捷键显示的逻辑、优化了两次测试间隔禁止按键的逻辑

--- public/Apps/ShortcutTester/ShortcutTester.js
+++ public/Apps/ShortcutTester/ShortcutTester.js
@@ -3,9 +3,14 @@ const root = document.querySelector(":root");
 let 姓名 = "";
 let 次数 = 10;
 let 剩余次数 = 次数;
+let 奇偶次数 = 剩余次数 % 2 ? 剩余次数 + 1 : 剩余次数;
 let 测试运行中 = false;
 let 旋转角度 = 0;
-const 旋转用时 = root.style.getPropertyValue("--旋转用时");
+const 旋转用时 = parseInt(
+  window.getComputedStyle(root).getPropertyValue("--旋转用时"),
+  10,
+);
+const 测试间隔 = 1000;
 let 旋转延时id = null;
 const 旋转容器 = document.querySelector(".测试容器-旋转");
 
@@ -15,14 +20,44 @@ const 列表项容器组 = document.querySelectorAll(".列表项容器");
 for (const [index, 容器] of 列表项容器组.entries()) {
   容器.id = `${index}`;
 }
+
+const 取消选择按钮 = document.getElementById("取消选择");
+const 取消测试按钮 = document.getElementById("cancel");
 const 开始按钮 = document.querySelector("#run");
+
 const 测试面组 = document.querySelectorAll(".测试面");
+let 当前用户快捷键内容 = null;
+
+let 错误动画 = null;
+const 错误动画关键帧序列 = [
+  { backgroundColor: "transparent" },
+  { backgroundColor: "brown", offset: 0.2 },
+  { backgroundColor: "transparent" },
+  { backgroundColor: "brown", offset: 0.8 },
+  { backgroundColor: "transparent" },
+];
+
+const 错误动画设置 = {
+  easing: "ease-out",
+  duration: 1000,
+};
 
 const 快捷键种类区 = document.querySelector(".快捷键种类区");
 const 快捷键列表区 = document.querySelector(".快捷键列表区");
 // 快捷键列表区.style.width = window.getComputedStyle(快捷键种类区).width;
 const 快捷键种类组 = 快捷键种类区.querySelectorAll(".快捷键种类");
 const 快捷键列表组 = 快捷键列表区.querySelectorAll(".快捷键列表");
+
+取消选择按钮.addEventListener("click", () => {
+  for (const 列表 of 快捷键列表组) {
+    const 已确认容器组 = 列表.querySelectorAll(".已确认容器");
+    for (const 已确认容器 of 已确认容器组) {
+      已确认容器.classList.remove("已确认容器");
+    }
+  }
+  测试对象池.length = 0;
+});
+
 let 当前快捷键种类 = 快捷键种类组[0];
 let 当前快捷键列表 = 快捷键列表组[0];
 for (const [索引, 种类] of 快捷键种类组.entries()) {
@@ -100,27 +135,33 @@ const 次数框 = document.getElementById("测试次数");
 次数框.addEventListener("input", 更新次数);
 
 开始按钮.addEventListener("click", () => {
-  测试运行中 = !测试运行中;
+  /*测试运行中 = !测试运行中;
 
   if (!测试运行中) {
     return;
-  }
+  }*/
 
-  if (测试对象池.length === 0 || 次数 <= 0) {
+  if (测试对象池.length === 0 || 姓名 === "" || 次数 <= 0) {
     生成错误信息();
     return;
   }
 
+  剩余次数 = 次数;
   测试对象组.length = 0;
   生成测试对象组();
 
-  测试面组[剩余次数 % 2].innerHTML = "";
-  测试面组[剩余次数 % 2].append(
+  奇偶次数 = 剩余次数 % 2 ? 剩余次数 + 1 : 剩余次数;
+  旋转容器.style.rotate = "";
+  旋转角度 = 0;
+
+  测试面组[奇偶次数 % 2].innerHTML = "";
+  测试面组[奇偶次数 % 2].append(
     生成测试目标(次数, 剩余次数),
     生成用户按键信息前缀(),
   );
 
   window.addEventListener("keydown", 按下快捷键封装器);
+  window.addEventListener("keyup", 松开快捷键封装器);
 
   /*window.addEventListener("keydown", (event) => {
     event.preventDefault();
@@ -225,8 +266,6 @@ const 次数框 = document.getElementById("测试次数");
       window.removeEventListener("keydown", 按下快捷键);
     }
   });*/
-
-  window.addEventListener("keyup", 松开快捷键);
 });
 
 const 按下快捷键封装器 = function (event) {
@@ -238,8 +277,10 @@ function 按下快捷键(event) {
   if (event.repeat) {
     return;
   }
+
   const 用户快捷键内容 =
-    测试面组[剩余次数 % 2].querySelector(".用户快捷键内容");
+    测试面组[奇偶次数 % 2].querySelector(".用户快捷键内容");
+  当前用户快捷键内容 = 用户快捷键内容;
   用户快捷键内容.innerHTML = "";
   const 描述 = document.createElement("span");
   描述.className = "测试描述";
@@ -302,8 +343,20 @@ function 按下快捷键(event) {
     } else if (ctrl && alt) {
       alt.after(meta);
       meta.before(生成连接符());
+    } else if (ctrl && shift) {
+      shift.after(meta);
+      meta.before(生成连接符());
+    } else if (alt && shift) {
+      shift.after(meta);
+      meta.before(生成连接符());
     } else if (ctrl) {
       ctrl.after(meta);
+      meta.before(生成连接符());
+    } else if (alt) {
+      alt.after(meta);
+      meta.before(生成连接符());
+    } else if (shift) {
+      shift.after(meta);
       meta.before(生成连接符());
     } else {
       描述.after(meta);
@@ -316,33 +369,66 @@ function 按下快捷键(event) {
     keyElement.usage = key.toUpperCase();
     keyElement.textContent = key.toUpperCase();
     用户快捷键内容.appendChild(keyElement);
-    keyElement.before(生成连接符());
+    const 功能键组 = 用户快捷键内容.querySelectorAll(".功能键");
+    if (功能键组.length > 0) {
+      keyElement.before(生成连接符());
+    }
 
     剩余次数--;
+    奇偶次数--;
     if (剩余次数 > 0) {
-      测试面组[剩余次数 % 2].innerHTML = "";
-      测试面组[剩余次数 % 2].append(
+      测试面组[奇偶次数 % 2].innerHTML = "";
+      测试面组[奇偶次数 % 2].append(
         生成测试目标(次数, 剩余次数),
         生成用户按键信息前缀(),
       );
       旋转角度 += 180;
       旋转延时id = setTimeout(() => {
         旋转容器.style.rotate = `y ${旋转角度}deg`;
-      }, 1000);
+      }, 测试间隔);
+    }
+
+    window.removeEventListener("keydown", 按下快捷键封装器);
+    window.removeEventListener("keyup", 松开快捷键封装器);
+    if (剩余次数 > 0) {
+      setTimeout(() => {
+        window.addEventListener("keydown", 按下快捷键封装器);
+        window.addEventListener("keyup", 松开快捷键封装器);
+      }, 旋转用时 + 测试间隔);
     }
   }
-
-  if (剩余次数 <= 0) {
-    window.removeEventListener("keydown", 按下快捷键封装器);
-  }
-
-  console.log(剩余次数);
 }
 
+const 松开快捷键封装器 = function (event) {
+  松开快捷键(event);
+};
+
 function 松开快捷键(event) {
-  if (剩余次数 <= 0) {
-    window.removeEventListener("keyup", 松开快捷键);
+  const 功能键组 = 当前用户快捷键内容?.querySelectorAll(".功能键");
+  const 功能键数组 = Array.from(功能键组);
+  const 按键组 = 当前用户快捷键内容?.querySelector(".按键");
+  const key = event.key;
+  let 键 = null;
+  if (key === "Shift") {
+    键 = 功能键数组.find((item) => item.textContent.trim() === "Shift");
+  } else if (key === "Control") {
+    键 = 功能键数组.find((item) => item.textContent.trim() === "Ctrl");
+  } else if (key === "Alt") {
+    键 = 功能键数组.find((item) => item.textContent.trim() === "Alt");
+  } else if (key === "Meta") {
+    键 = 功能键数组.find((item) => item.textContent.trim() === "Win");
   }
+  const 前连接符 = 键?.previousElementSibling;
+  const 后连接符 = 键?.nextElementSibling;
+  const 键索引 = 功能键数组.indexOf(键);
+  if (键索引 > 0 && 键索引 < 功能键数组.length - 1) {
+    前连接符?.remove();
+  } else if (键索引 === 0) {
+    后连接符?.remove();
+  } else if (键索引 === 功能键数组.length - 1) {
+    前连接符?.remove();
+  }
+  键?.remove();
 }
 
 function 生成测试对象组() {
@@ -442,9 +528,25 @@ function 生成连接符() {
 function 生成错误信息() {
   const 错误文字层 = document.querySelector(".错误文字层");
   const 错误动画层 = document.querySelector(".错误动画层");
+  错误动画?.cancel();
   if (测试对象池.length === 0) {
-    错误文字层.textContent = "没有选择任何快捷键";
-  } else if (次数 === 0) {
-    错误文字层.textContent = "测试次数为0";
+    const 快捷键列表区 = document.querySelector(".快捷键列表区");
+    错误动画 = 快捷键列表区.animate(错误动画关键帧序列, 错误动画设置);
+  } else if (姓名 === "") {
+    错误动画 = 姓名框.animate(错误动画关键帧序列, 错误动画设置);
+  } else if (次数 <= 0) {
+    次数框.animate(错误动画关键帧序列, 错误动画设置);
   }
 }
+
+取消测试按钮.addEventListener("click", () => {
+  测试运行中 = false;
+  测试面组[0].innerHTML = "";
+  测试面组[1].innerHTML = "";
+  旋转容器.style.transition = "none";
+  旋转容器.style.rotate = "";
+  setTimeout(() => {
+    旋转容器.style.transition = "";
+  }, 旋转用时);
+  旋转角度 = 0;
+});
