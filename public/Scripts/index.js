478595120a53eadf0bee8112b3bf164e74a61acf
suyangzuo <suyangzuo@163.com>
2025-06-13 16:04:39 +0800

    完成主页的第三方教程与软件分区

--- public/Scripts/index.js
+++ public/Scripts/index.js
@@ -1,10 +1,7 @@
 const App和游戏导航区 = document.querySelector(".App和游戏导航区");
-const App和游戏导航区高度 = window.getComputedStyle(App和游戏导航区).height;
 const App和游戏导航分区组 = App和游戏导航区.querySelectorAll(".App和游戏导航分区");
 const App和游戏导航区背景 = App和游戏导航区.querySelector(".App和游戏导航区背景");
 const App和游戏范例区 = document.querySelector(".App和游戏范例区");
-App和游戏范例区.style.height = App和游戏导航区高度;
-
 const App和游戏范例分区组 = App和游戏范例区.querySelectorAll(".范例分区");
 
 for (const [index, 导航分区] of App和游戏导航分区组.entries()) {
